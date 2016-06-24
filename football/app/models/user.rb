28a64a77fd28d729e5a540822b14f772b22d6710
pdkproitf <phucdaica.pro@gMAIL.com>
2016-06-24 16:24:02 +0700

    Add more Test on  User model  but cannot secure passwork

--- football/app/models/user.rb
+++ football/app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
                     presence: true,
                     uniqueness: {message: "alrealy exits"}
+  validates :smartphone, numericality: {only_integer: true}
+
+  validates :name, presence: true, length: { minimum:  8}
+  validates :password, presence: true, confirmation: true,length: { minimum: 8 }
 
 end
--- football/spec/models/user_spec.rb
+++ football/spec/models/user_spec.rb
@@ -2,15 +2,32 @@ require 'rails_helper'
 
 RSpec.describe :User, type: :model do
 
-  describe "validate email" do 
-    before(:each) do
+  before(:all) do
       @user = FactoryGirl.build(:user)
     end
-    subject { @user }
+  subject { @user }
+
+  describe "validate email" do 
 
     it { @user.should_not be_nil }
   	it { should validate_presence_of :email}
 
+  end
+
+  describe "validate smartphone" do 
+    it { should validate_numericality_of(:smartphone).only_integer }
+  end
+
+  describe "validate name" do
+    it { should validate_presence_of :name }
+    it { should validate_length_of :name }
+  end
+
+  describe "validate password" do
+    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password }
+    it { should validate_length_of :password }
+    # it { should have_secure_password }   ********** chua lam dc 
   end
 
 end
